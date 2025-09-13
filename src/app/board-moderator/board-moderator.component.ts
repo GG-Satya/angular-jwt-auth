fd1766bb6d1730cd10ccaecd357ba4160c5d4bd7
GG-Satya <dsatyaprasanna@gmail.com>
2025-09-13 18:57:58 +0530

    Upgraded to Angular 19

--- src/app/board-moderator/board-moderator.component.ts
+++ src/app/board-moderator/board-moderator.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
 
 @Component({
-  selector: 'app-board-moderator',
-  templateUrl: './board-moderator.component.html',
-  styleUrls: ['./board-moderator.component.css']
+    selector: 'app-board-moderator',
+    templateUrl: './board-moderator.component.html',
+    styleUrls: ['./board-moderator.component.css'],
+    standalone: false
 })
 export class BoardModeratorComponent implements OnInit {
   content?: string;
